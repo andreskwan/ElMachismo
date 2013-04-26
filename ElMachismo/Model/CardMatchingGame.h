4b122497d5e443ebfcfdbea9edf205c33c36a787
ANDRES KWAN <andreskwan@gmail.com>
2013-04-26 08:04:14 -0500

    v3-demo CardMatchinGame - create class

---
+++ ElMachismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  ElMachismo
+//
+//  Created by Andres Kwan on 4/26/13.
+//  Copyright (c) 2013 com.stanfor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ ElMachismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  ElMachismo
+//
+//  Created by Andres Kwan on 4/26/13.
+//  Copyright (c) 2013 com.stanfor. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
