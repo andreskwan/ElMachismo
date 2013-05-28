a92526aef783671251a888a470333f6fc3c3ea91
ANDRES KWAN <andreskwan@gmail.com>
2013-05-28 11:20:40 -0500

    CardGameViewController - fitxing After merge

--- ElMachismo/CardGameViewController.m
+++ ElMachismo/CardGameViewController.m
@@ -20,8 +20,6 @@
 
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray  *cardButtons;
 
-
-
 //MODEL - it's very frequent to have a property that points to the model
 @property (strong, nonatomic) CardMatchingGame                      *game;
 @end
@@ -34,27 +32,15 @@
     //no strong pointer to the deck, is just needed to take the playing cards, no more
     if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
                                                          usingDeck:[[PlayingCardDeck alloc]init]];
-<<<<<<< HEAD
     return _game;
-=======
+
 }
 
-//common method, used to make the view, match the model.
-//common paradime
-- (void)updateUI
-{
-    
->>>>>>> 552110a9615ccf3c2785f1926d97a7fd1ec3c8ed
-}
 
 - (void)setCardButtons:(NSArray *)cardButtons
 {
     //its kind of magic hear how cardButtons are instantiated!!! Now I understand!
     _cardButtons = cardButtons;
-<<<<<<< HEAD
-=======
-
->>>>>>> 552110a9615ccf3c2785f1926d97a7fd1ec3c8ed
     [self updateUI];
     
 }
@@ -64,7 +50,6 @@
 //common paradime
 - (void)updateUI
 {
-<<<<<<< HEAD
     for (UIButton * cardButton in self.cardButtons) {
         Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
         //the card did match but are not enable any more so we have to add
@@ -75,13 +60,7 @@
         cardButton.alpha = (card.isUnplayable ? 0.3 : 1.0);
     }
     
-=======
-        //this is wrong, because is the model which decides wich buttons are in the selected state
-    //tell my model
-    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
-    sender.selected = !sender.isSelected;
-    self.flipcount++;
->>>>>>> 552110a9615ccf3c2785f1926d97a7fd1ec3c8ed
+//self.
 }
 
 
