99ee91aa0e05a2cb75abaa02bf578858515abe03
ANDRES KWAN <andreskwan@gmail.com>
2013-05-30 11:01:48 -0500

    v5 NSUserDefault - how to use it

--- ElMachismo/GameResult.m
+++ ElMachismo/GameResult.m
@@ -33,10 +33,53 @@
     return [self.end timeIntervalSinceDate:self.start];
 }
 
+//GameResult_ALL to namespace that constant
+//to avoid conflicts with other keys "ALL is a bad name"
+//this is the key that is going to be directly store in NSUserDefault
+//its value is a dictionary that has other dictionaries in it.
+#define ALL_RESULTS_KEY @"GameResult_All"
 
+//used to write results out in the NSUserDefauls
+//GameResult is not a property list, we have to turn it into.
+- (void) synchronize
+{
+    //first TAKE IT OUT, make a mutalbe copy of it
+    //how we access NSUserDefaults
+    
+    NSMutableDictionary *mutableGameResultsFromUserDefaults =
+    [[[NSUserDefaults standardUserDefaults]dictionaryForKey:ALL_RESULTS_KEY]mutableCopy];
+    
+    if (!mutableGameResultsFromUserDefaults) {
+        mutableGameResultsFromUserDefaults = [[NSMutableDictionary alloc]init];
+    }
+    //Second
+    //modify it
+    mutableGameResultsFromUserDefaults[[self.start description]] = [self asPropertyList];
+    
+    //third
+    //put it back in
+    [[NSUserDefaults standardUserDefaults] setObject:mutableGameResultsFromUserDefaults forKey:ALL_RESULTS_KEY];
+    
+    //fourth
+    //synchronize
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+}
+
+#define START_KEY @"StartDate"
+#define END_KEY @"EndDate"
+#define SCORE_KEY @"Score"
+
+- (id)asPropertyList
+{
+    return @{ START_KEY : self.start, END_KEY : self.end, SCORE_KEY: @(self.score)  };
+}
 - (void)setScore:(int)score
 {
     _score = score;
     self.end = [NSDate date];
+    [self synchronize];
 }
+
+
 @end
