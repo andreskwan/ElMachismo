816f9418000e4143f19835a9ea9e1e36cdaf705d
ANDRES KWAN <andreskwan@gmail.com>
2013-05-30 10:05:46 -0500

    v5 GameResult - helper for GameResutlVC - this should be done in the model

---
+++ ElMachismo/GameResult.h
@@ -0,0 +1,13 @@
+//
+//  GameResult.h
+//  ElMachismo
+//
+//  Created by Andres Kwan on 5/30/13.
+//  Copyright (c) 2013 com.stanfor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResult : NSObject
+
+@end
---
+++ ElMachismo/GameResult.m
@@ -0,0 +1,13 @@
+//
+//  GameResult.m
+//  ElMachismo
+//
+//  Created by Andres Kwan on 5/30/13.
+//  Copyright (c) 2013 com.stanfor. All rights reserved.
+//
+
+#import "GameResult.h"
+
+@implementation GameResult
+
+@end
