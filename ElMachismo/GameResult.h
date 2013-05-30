0f3caa4f0b9ffcd42ea1ca5a8c7e15c67326729a
ANDRES KWAN <andreskwan@gmail.com>
2013-05-30 10:21:28 -0500

    v5 GameResult - implementation

--- ElMachismo/GameResult.h
+++ ElMachismo/GameResult.h
@@ -10,4 +10,14 @@
 
 @interface GameResult : NSObject
 
+//when this class is instantiated the game start
+@property (readonly, nonatomic)NSDate           *start;
+
+//every time someone update the score, the game is ended
+@property (readonly, nonatomic)NSDate           *end;
+
+@property (readonly, nonatomic)NSTimeInterval   duration;
+
+//is not readonly because you can change it!
+@property (nonatomic) int score;
 @end
--- ElMachismo/GameResult.m
+++ ElMachismo/GameResult.m
@@ -8,6 +8,35 @@
 
 #import "GameResult.h"
 
+@interface GameResult()
+@property (readwrite, nonatomic) NSDate *start;
+@property (readwrite, nonatomic) NSDate *end;
+
+@end
+
 @implementation GameResult
 
+- (id) init
+{
+    self = [super init];
+    if (self) {
+        //don't use setters and getters in initializers
+        //specialy in my designated initializer
+        _start = [NSDate date];
+        _end   = _start;
+    }
+    return self;
+}
+
+- (NSTimeInterval) duration
+{
+    return [self.end timeIntervalSinceDate:self.start];
+}
+
+
+- (void)setScore:(int)score
+{
+    _score = score;
+    self.end = [NSDate date];
+}
 @end
